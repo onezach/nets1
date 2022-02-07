fb922d07488f82016a06ac760c54fea5dd77a0f3
pat-loughran <71353422+pat-loughran@users.noreply.github.com>
2022-02-06 20:35:44 -0600

    added some server code

--- Iperfer.java
+++ Iperfer.java
@@ -1,20 +1,56 @@
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Iperfer {
     public static void main(String[] args) {
-        if (args.length != 9) {
-            System.out.println("Error");
-            System.out.println("Patrick edit");
+ 
+        if (args.length > 0 && args[0].equals("-s")) {
+            server(args);
         }
     }
 
-    public void client() {
+    public static void client() {
 
 
         return;
     }
 
-    public void server() {
+    public static void server(String[] args) {
+        // command line checks
+        if (args.length != 3) {
+            System.out.println("Error: missing or additional arguments");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[2]);
+        if (port < 1024 || port > 65535) {
+            System.out.println("Error: port number must be in the range 1024 to 65535");
+            System.exit(1);
+        }
+
+        // open server-side socket
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            InputStream in = clientSocket.getInputStream();
+        ) 
+        {
+            //inside try block
+            int rv;
+            int bytes_read = 0;
+            while((rv = in.read(new byte[1000])) != -1) {
+                bytes_read += rv;
+                // Ask TA: if connection is closed, won't I/O error prevent rv from updating?
+            }
+            
+
+        } catch (IOException e) {
+            System.exit(1);
+        }
+
         return;
     }
 }
