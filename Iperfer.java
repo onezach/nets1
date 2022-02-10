0bfef3dfb9012397cf26e261f3da803c32c13c39
pat-loughran <71353422+pat-loughran@users.noreply.github.com>
2022-02-10 15:20:32 -0600

    new byte size

--- Iperfer.java
+++ Iperfer.java
@@ -50,8 +50,8 @@ public class Iperfer {
                 counter++;
             }
 
-            double mbps = (counter * 8) / (seconds * 1000); 
-            // kilobytes * 1000 --> bytes * 8 --> bits / 1000000 --> megabits / seconds --> megabits/sec
+            double mbps = (counter * 8 * 1024) / (seconds * 1000000); 
+            // kilobytes * 1024 --> bytes * 8 --> bits / 1000000 --> megabits / seconds --> megabits/sec
             System.out.println("sent=" + counter + " KB rate=" + mbps + " Mbps");
         }
 
@@ -92,9 +92,9 @@ public class Iperfer {
 
             long end = System.currentTimeMillis();
             long duration = (end - start) / 1000;
-            double megabits = (bytes_read * 8) / 1000000;
+            long megabits = (bytes_read * 8) / 1000000;
             double mbps = megabits / duration;
-            double KB_read = bytes_read / 1024;
+            long KB_read = bytes_read / 1000;
 
             System.out.println("recieved=" + (int) KB_read + " KB rate=" + mbps + " Mbps");
 
